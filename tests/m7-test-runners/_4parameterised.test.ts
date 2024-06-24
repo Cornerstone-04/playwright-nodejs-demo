e407a89693d5a53d73f4880e27afeae859b8c64f
Cornerstone-04 <fortunecornerstone@gmail.com>
2024-06-24 14:58:38 +0100

    feat: Module 7 - test parameterisation

--- tests/m7-test-runners/_4parameterised.test.ts
+++ tests/m7-test-runners/_4parameterised.test.ts
@@ -1,9 +1,35 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 const people = ["Alice", "John"];
 
 for (const name of people) {
   test(`Test ${name}`, async () => {
     console.log(name);
+  });
+}
+
+const map1 = new Map();
+map1.set(2, 20);
+map1.set(3, 30);
+
+for (const [key, value] of map1) {
+  test(`Testing 10x function with ${key} and ${value}`, async () => {
+    expect(key * 10).toEqual(value);
+  });
+}
+
+const inputs = [
+  ["10", "6 months", "After 6 Months you will earn $0.20 on your deposit"],
+  ["20", "1 Year", "After 1 Year you will earn $1.00 on your deposit"],
+];
+
+for (const [sum, period, result] of inputs) {
+  test(`Testing deposit calculator with ${sum} and ${period}`, async ({
+    page,
+  }) => {
+    await page.goto("/savings.html");
+    await page.getByTestId("deposit").fill(sum);
+    await page.getByTestId("period").selectOption(period);
+    await expect(page.getByTestId("result")).toHaveText(result);
   });
 }
